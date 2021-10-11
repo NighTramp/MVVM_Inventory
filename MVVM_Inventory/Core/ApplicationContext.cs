d34dee869d6c3f25083b480e94ff7398b8788448
Георгий Реснянский <georg2498@mail.ru>
2021-10-11 11:38:47 +0400

    Добавьте файлы проекта.

---
+++ MVVM_Inventory/Core/ApplicationContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+using MVVM_Inventory.MVVM.Model;
+
+namespace MVVM_Inventory.Core
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext() : base("MVVM_Inventory.Properties.Settings.DBIConnectionString") { }
+    }
+}
