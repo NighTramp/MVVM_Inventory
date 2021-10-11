73404f8111f0f51fd4111d9517f0fb4d6d17bf45
Георгий Реснянский <georg2498@mail.ru>
2021-10-11 17:29:47 +0400

    Глобальная переработка подхода к организации приложения. Переход к паттерну MVVM.

---
+++ MVVM_Inventory/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVM_Inventory.Core;
+
+namespace MVVM_Inventory.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public AuthViewModel AuthVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            AuthVM = new AuthViewModel();
+            CurrentView = AuthVM;
+        }
+
+    }
+}
